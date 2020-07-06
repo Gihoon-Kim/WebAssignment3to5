6ebf84888999340079b42877a49971267900592a
Gihoon Kim <gkim61@myseneca.ca>
2020-07-06 07:13:32 -0400

    almost done

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ app.get("/dashboard", (req, res) => {
     });
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> {
     console.log(`The webserver is up and running`);
